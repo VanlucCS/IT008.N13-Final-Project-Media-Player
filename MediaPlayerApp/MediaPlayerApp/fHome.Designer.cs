fbdd6351ad76661daba9989c4653726a0e6e1839
Trần Văn Lực <99704547+VanlucCS@users.noreply.github.com>
2023-01-03 09:06:35 +0700

    feat : header button

--- MediaPlayerApp/MediaPlayerApp/fHome.Designer.cs
+++ MediaPlayerApp/MediaPlayerApp/fHome.Designer.cs
@@ -31,11 +31,16 @@ namespace MediaPlayerApp
         {
             this.components = new System.ComponentModel.Container();
             this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
-            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.pnControlMedia = new Guna.UI2.WinForms.Guna2GradientPanel();
             this.guna2TrackBar1 = new Guna.UI2.WinForms.Guna2TrackBar();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.guna2GradientPanel1.SuspendLayout();
+            this.pnHeader = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox3 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.pnControlMedia.SuspendLayout();
+            this.pnHeader.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2BorderlessForm1
@@ -45,18 +50,18 @@ namespace MediaPlayerApp
             this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.4D;
             this.guna2BorderlessForm1.TransparentWhileDrag = true;
             // 
-            // guna2GradientPanel1
+            // pnControlMedia
             // 
-            this.guna2GradientPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.guna2GradientPanel1.Controls.Add(this.label2);
-            this.guna2GradientPanel1.Controls.Add(this.label1);
-            this.guna2GradientPanel1.Controls.Add(this.guna2TrackBar1);
-            this.guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.guna2GradientPanel1.Location = new System.Drawing.Point(0, 567);
-            this.guna2GradientPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
-            this.guna2GradientPanel1.Size = new System.Drawing.Size(1000, 83);
-            this.guna2GradientPanel1.TabIndex = 0;
+            this.pnControlMedia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pnControlMedia.Controls.Add(this.label2);
+            this.pnControlMedia.Controls.Add(this.label1);
+            this.pnControlMedia.Controls.Add(this.guna2TrackBar1);
+            this.pnControlMedia.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnControlMedia.Location = new System.Drawing.Point(0, 567);
+            this.pnControlMedia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnControlMedia.Name = "pnControlMedia";
+            this.pnControlMedia.Size = new System.Drawing.Size(1000, 83);
+            this.pnControlMedia.TabIndex = 0;
             // 
             // guna2TrackBar1
             // 
@@ -90,20 +95,66 @@ namespace MediaPlayerApp
             this.label2.TabIndex = 2;
             this.label2.Text = "00:00";
             // 
+            // pnHeader
+            // 
+            this.pnHeader.Controls.Add(this.guna2ControlBox3);
+            this.pnHeader.Controls.Add(this.guna2ControlBox2);
+            this.pnHeader.Controls.Add(this.guna2ControlBox1);
+            this.pnHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnHeader.Name = "pnHeader";
+            this.pnHeader.Size = new System.Drawing.Size(1000, 40);
+            this.pnHeader.TabIndex = 1;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.CustomIconSize = 15F;
+            this.guna2ControlBox1.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(944, 5);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(893, 5);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 1;
+            // 
+            // guna2ControlBox3
+            // 
+            this.guna2ControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox3.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+            this.guna2ControlBox3.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2ControlBox3.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox3.Location = new System.Drawing.Point(842, 5);
+            this.guna2ControlBox3.Name = "guna2ControlBox3";
+            this.guna2ControlBox3.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox3.TabIndex = 2;
+            // 
             // fHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 650);
-            this.Controls.Add(this.guna2GradientPanel1);
+            this.Controls.Add(this.pnHeader);
+            this.Controls.Add(this.pnControlMedia);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "fHome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Media Player";
-            this.guna2GradientPanel1.ResumeLayout(false);
-            this.guna2GradientPanel1.PerformLayout();
+            this.pnControlMedia.ResumeLayout(false);
+            this.pnControlMedia.PerformLayout();
+            this.pnHeader.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -111,10 +162,14 @@ namespace MediaPlayerApp
         #endregion
 
         private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
-        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Guna.UI2.WinForms.Guna2GradientPanel pnControlMedia;
         private System.Windows.Forms.Label label1;
         private Guna.UI2.WinForms.Guna2TrackBar guna2TrackBar1;
         private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2GradientPanel pnHeader;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox3;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
     }
 }
 
