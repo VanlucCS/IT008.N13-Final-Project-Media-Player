05fd3b3784ce97e60805f17d315156360c87ec43
Trần Văn Lực <99704547+VanlucCS@users.noreply.github.com>
2023-01-04 22:02:46 +0700

    set scale form

--- MediaPlayerApp/MediaPlayerApp/UI/fFavorite.Designer.cs
+++ MediaPlayerApp/MediaPlayerApp/UI/fFavorite.Designer.cs
@@ -33,8 +33,7 @@ namespace MediaPlayerApp.UI
             // 
             // fFavorite
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(788, 510);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "fFavorite";
