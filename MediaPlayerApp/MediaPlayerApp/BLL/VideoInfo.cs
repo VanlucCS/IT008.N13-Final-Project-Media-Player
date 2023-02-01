3ade101bbda128a46f184dcfd8288ab5217938f5
Trần Văn Lực <99704547+VanlucCS@users.noreply.github.com>
2023-02-01 20:36:13 +0700

    add package

---
+++ MediaPlayerApp/MediaPlayerApp/BLL/VideoInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayerApp.BLL
+{
+    class VideoInfo
+    {
+        public string Name;
+        public string Date;
+        public string Size;
+        public string Length;
+        public string link_Video;
+        //public Image picture_Video;
+    }
+}
