797ea45b75a0fd94fb347d74a543cdd13f929d65
Trần Văn Lực <99704547+VanlucCS@users.noreply.github.com>
2023-02-01 17:02:21 +0700

    dev: UI Fvor song

--- MediaPlayerApp/MediaPlayerApp/UI/fFavorite.cs
+++ MediaPlayerApp/MediaPlayerApp/UI/fFavorite.cs
@@ -18,5 +18,35 @@ namespace MediaPlayerApp.UI
             this.parent = parent;
             InitializeComponent();
         }
+
+        private void moreButton_Click(object sender, EventArgs e)
+        {
+            //thumbnailMenu.MaximumSize = new Size(200, 200);
+            //thumbnailMenu.Width = 190;
+            //thumbnailMenu.BringToFront();
+            //thumbnailMenu.showHide();
+            ContextMenuStrip toolStrip = new ContextMenuStrip();
+            toolStrip.ItemClicked += menuClick;
+            toolStrip.Items.Clear();
+            toolStrip.LayoutStyle = ToolStripLayoutStyle.VerticalStackWithOverflow;
+            toolStrip.Items.Add("Delete all");
+            toolStrip.Items.Add("2");
+            toolStrip.Items.Add("-");
+            toolStrip.Items.Add("3");
+            int Y = 400;
+            toolStrip.Location = new Point(moreButton.Location.X + 70, moreButton.Location.Y + Y);
+            toolStrip.Show(MousePosition);
+            toolStrip.BringToFront();
+            toolStrip.GripStyle = ToolStripGripStyle.Hidden;
+            toolStrip.Dock = DockStyle.None;
+        }
+        private void menuClick(object sender, ToolStripItemClickedEventArgs e)
+        {
+            // click..
+            if (e.ClickedItem.Text == "1")
+            {
+                MessageBox.Show("1");
+            }
+        }
     }
 }
