ea96913b0272d234e1d82072cf6176abafc840fd
Ceci <81894687+Ceci-june@users.noreply.github.com>
2023-02-05 00:22:06 +0700

    add: play button and progress bar to fVideo

--- MediaPlayerApp/MediaPlayerApp/UI/fVideo.Designer.cs
+++ MediaPlayerApp/MediaPlayerApp/UI/fVideo.Designer.cs
@@ -28,53 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fVideo));
-            this.player = new AxWMPLib.AxWindowsMediaPlayer();
-            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlHead = new Guna.UI2.WinForms.Guna2Panel();
             this.btMaxsize = new Guna.UI2.WinForms.Guna2ControlBox();
             this.btMinisize = new Guna.UI2.WinForms.Guna2ControlBox();
             this.btExit = new Guna.UI2.WinForms.Guna2ControlBox();
-            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlBottom = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbTimeCurrentPlay = new System.Windows.Forms.Label();
+            this.tbProcess = new Guna.UI2.WinForms.Guna2TrackBar();
             this.btPlay = new Guna.UI2.WinForms.Guna2ImageButton();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
-            this.guna2Panel1.SuspendLayout();
-            this.guna2Panel2.SuspendLayout();
+            this.timer1s = new System.Windows.Forms.Timer(this.components);
+            this.timer5s = new System.Windows.Forms.Timer(this.components);
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pnlHead.SuspendLayout();
+            this.pnlBottom.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             this.SuspendLayout();
             // 
-            // player
+            // pnlHead
             // 
-            this.player.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.player.Enabled = true;
-            this.player.Location = new System.Drawing.Point(0, 0);
-            this.player.Name = "player";
-            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
-            this.player.Size = new System.Drawing.Size(800, 450);
-            this.player.TabIndex = 0;
-            this.player.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.player_PlayStateChange);
-            // 
-            // guna2Panel1
-            // 
-            this.guna2Panel1.Controls.Add(this.btMaxsize);
-            this.guna2Panel1.Controls.Add(this.btMinisize);
-            this.guna2Panel1.Controls.Add(this.btExit);
-            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
-            this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(800, 35);
-            this.guna2Panel1.TabIndex = 1;
+            this.pnlHead.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pnlHead.Controls.Add(this.btMaxsize);
+            this.pnlHead.Controls.Add(this.btMinisize);
+            this.pnlHead.Controls.Add(this.btExit);
+            this.pnlHead.Location = new System.Drawing.Point(0, 0);
+            this.pnlHead.Name = "pnlHead";
+            this.pnlHead.Size = new System.Drawing.Size(800, 35);
+            this.pnlHead.TabIndex = 1;
+            this.pnlHead.Visible = false;
             // 
             // btMaxsize
             // 
             this.btMaxsize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btMaxsize.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
-            this.btMaxsize.FillColor = System.Drawing.SystemColors.Control;
+            this.btMaxsize.FillColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btMaxsize.IconColor = System.Drawing.Color.Black;
             this.btMaxsize.Location = new System.Drawing.Point(688, 3);
             this.btMaxsize.Name = "btMaxsize";
@@ -85,7 +81,7 @@
             // 
             this.btMinisize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btMinisize.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
-            this.btMinisize.FillColor = System.Drawing.SystemColors.Control;
+            this.btMinisize.FillColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btMinisize.IconColor = System.Drawing.Color.Black;
             this.btMinisize.Location = new System.Drawing.Point(639, 3);
             this.btMinisize.Name = "btMinisize";
@@ -96,7 +92,7 @@
             // 
             this.btExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btExit.CustomIconSize = 15F;
-            this.btExit.FillColor = System.Drawing.SystemColors.Control;
+            this.btExit.FillColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btExit.IconColor = System.Drawing.Color.Black;
             this.btExit.Location = new System.Drawing.Point(739, 3);
             this.btExit.Name = "btExit";
@@ -104,17 +100,50 @@
             this.btExit.TabIndex = 3;
             this.btExit.Click += new System.EventHandler(this.btExit_Click);
             // 
-            // guna2Panel2
+            // pnlBottom
             // 
-            this.guna2Panel2.Controls.Add(this.btPlay);
-            this.guna2Panel2.Controls.Add(this.pictureBox4);
-            this.guna2Panel2.Controls.Add(this.pictureBox3);
-            this.guna2Panel2.Controls.Add(this.pictureBox2);
-            this.guna2Panel2.Controls.Add(this.pictureBox1);
-            this.guna2Panel2.Location = new System.Drawing.Point(0, 350);
-            this.guna2Panel2.Name = "guna2Panel2";
-            this.guna2Panel2.Size = new System.Drawing.Size(800, 100);
-            this.guna2Panel2.TabIndex = 2;
+            this.pnlBottom.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pnlBottom.Controls.Add(this.lbTimeCurrentPlay);
+            this.pnlBottom.Controls.Add(this.tbProcess);
+            this.pnlBottom.Controls.Add(this.btPlay);
+            this.pnlBottom.Controls.Add(this.pictureBox4);
+            this.pnlBottom.Controls.Add(this.pictureBox3);
+            this.pnlBottom.Controls.Add(this.pictureBox2);
+            this.pnlBottom.Controls.Add(this.pictureBox1);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 350);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(800, 100);
+            this.pnlBottom.TabIndex = 2;
+            this.pnlBottom.Visible = false;
+            // 
+            // lbTimeCurrentPlay
+            // 
+            this.lbTimeCurrentPlay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbTimeCurrentPlay.AutoSize = true;
+            this.lbTimeCurrentPlay.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimeCurrentPlay.Location = new System.Drawing.Point(9, 10);
+            this.lbTimeCurrentPlay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTimeCurrentPlay.Name = "lbTimeCurrentPlay";
+            this.lbTimeCurrentPlay.Size = new System.Drawing.Size(88, 25);
+            this.lbTimeCurrentPlay.TabIndex = 21;
+            this.lbTimeCurrentPlay.Text = "00:00:00";
+            // 
+            // tbProcess
+            // 
+            this.tbProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbProcess.FillColor = System.Drawing.Color.White;
+            this.tbProcess.Location = new System.Drawing.Point(76, 0);
+            this.tbProcess.Margin = new System.Windows.Forms.Padding(4);
+            this.tbProcess.Name = "tbProcess";
+            this.tbProcess.Size = new System.Drawing.Size(708, 28);
+            this.tbProcess.Style = Guna.UI2.WinForms.Enums.TrackBarStyle.Metro;
+            this.tbProcess.TabIndex = 20;
+            this.tbProcess.ThumbColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
+            this.tbProcess.Value = 0;
+            this.tbProcess.Scroll += new System.Windows.Forms.ScrollEventHandler(this.tbProcess_Scroll);
             // 
             // btPlay
             // 
@@ -130,6 +159,7 @@
             this.btPlay.PressedState.ImageSize = new System.Drawing.Size(50, 50);
             this.btPlay.Size = new System.Drawing.Size(64, 54);
             this.btPlay.TabIndex = 19;
+            this.btPlay.Click += new System.EventHandler(this.btPlay_Click);
             // 
             // pictureBox4
             // 
@@ -152,6 +182,7 @@
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 15;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // pictureBox2
             // 
@@ -175,32 +206,54 @@
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
             // 
+            // timer1s
+            // 
+            this.timer1s.Enabled = true;
+            this.timer1s.Interval = 1000;
+            this.timer1s.Tick += new System.EventHandler(this.timer1s_Tick);
+            // 
+            // timer5s
+            // 
+            this.timer5s.Interval = 5000;
+            this.timer5s.Tick += new System.EventHandler(this.timer5s_Tick);
+            // 
+            // player
+            // 
+            this.player.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(0, 0);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(800, 450);
+            this.player.TabIndex = 3;
+            this.player.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.player_PlayStateChange);
+            this.player.MouseMoveEvent += new AxWMPLib._WMPOCXEvents_MouseMoveEventHandler(this.player_MouseMoveEvent);
+            // 
             // fVideo
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.guna2Panel2);
-            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.pnlBottom);
+            this.Controls.Add(this.pnlHead);
             this.Controls.Add(this.player);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "fVideo";
             this.Text = "fVideo";
-            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
-            this.guna2Panel1.ResumeLayout(false);
-            this.guna2Panel2.ResumeLayout(false);
+            this.pnlHead.ResumeLayout(false);
+            this.pnlBottom.ResumeLayout(false);
+            this.pnlBottom.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private AxWMPLib.AxWindowsMediaPlayer player;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Panel pnlHead;
+        private Guna.UI2.WinForms.Guna2Panel pnlBottom;
         private Guna.UI2.WinForms.Guna2ControlBox btMaxsize;
         private Guna.UI2.WinForms.Guna2ControlBox btMinisize;
         private Guna.UI2.WinForms.Guna2ControlBox btExit;
@@ -209,5 +262,10 @@
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbTimeCurrentPlay;
+        private System.Windows.Forms.Timer timer1s;
+        private System.Windows.Forms.Timer timer5s;
+        private Guna.UI2.WinForms.Guna2TrackBar tbProcess;
+        private AxWMPLib.AxWindowsMediaPlayer player;
     }
 }
--- MediaPlayerApp/MediaPlayerApp/UI/fVideo.cs
+++ MediaPlayerApp/MediaPlayerApp/UI/fVideo.cs
@@ -1,5 +1,7 @@
 ﻿using MediaPlayerApp.BLL;
 using MediaPlayerApp.Components;
+using MediaPlayerApp.Properties;
+using Microsoft.VisualBasic.Devices;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,6 +9,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Versioning;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,6 +26,7 @@ namespace MediaPlayerApp.UI
         private int index = 0;
         private bool replay = false;
         private bool shuffle = false;
+        private int X =0, Y = 0;
         public fVideo(fHome parent = null, Thumbnail thumbnail = null)
         {
             this.parent = parent;
@@ -39,12 +43,73 @@ namespace MediaPlayerApp.UI
             this.player.URL = videos[0].VideoPath;
             index = 0;
             playAll = true;
+            
+            //this.player
         }
 
         private void btExit_Click(object sender, EventArgs e)
         {
+            timer1s.Stop();
             player.Dispose();
             this.Close();
+        }
+
+
+
+        private void btPlay_Click(object sender, EventArgs e)
+        {
+            btPlay.Checked = !btPlay.Checked;
+            if (btPlay.Checked == true)
+            {
+                this.player.Ctlcontrols.play();
+                btPlay.Image = Resources.play_96px;
+            }
+            else
+            {
+                this.player.Ctlcontrols.pause();
+                btPlay.Image = Resources.pause;
+            }
+        }
+
+
+
+        private void tbProcess_Scroll(object sender, ScrollEventArgs e)
+        {
+            try
+            {
+                this.player.Ctlcontrols.currentPosition = (double)tbProcess.Value * this.player.currentMedia.duration / 100.0;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void timer1s_Tick(object sender, EventArgs e)
+        {
+            if (player.currentMedia.duration != 0)
+            {
+                TimeSpan Time = TimeSpan.FromMinutes(player.Ctlcontrols.currentPosition);
+                // update time mediaplayer
+                lbTimeCurrentPlay.Text = Time.ToString().Substring(0, 5);
+                tbProcess.Value = (int)(100.0 * (player.Ctlcontrols.currentPosition / player.currentMedia.duration));
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void player_MouseMoveEvent(object sender, AxWMPLib._WMPOCXEvents_MouseMoveEvent e)
+        {
+            if (e.fX != X || e.fY != Y)
+            {
+                timer5s.Enabled = true;
+                pnlBottom.Visible = true;
+                pnlHead.Visible = true;
+                X = e.fX;
+                Y = e.fY;
+            }
         }
 
         private void player_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
@@ -67,6 +132,13 @@ namespace MediaPlayerApp.UI
 
                 }
             }
+        }
+
+        private void timer5s_Tick(object sender, EventArgs e)
+        {
+            pnlBottom.Visible = false;
+            pnlHead.Visible = false;
+            timer5s.Enabled = false;
         }
     }
 }
